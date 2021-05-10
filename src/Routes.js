27aac8666672e232baae490f67ecd07ac92a80ef
yurim <icho0405@naver.com>
2021-05-10 17:00:04 +0900

    Add: first commit. 초기 세팅 완료.

---
+++ src/Routes.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Login from './pages/Login/Login';
+import Main from './pages/Main/Main';
+
+class Routes extends React.Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Main} />
+          <Route exact path="/login" component={Login} />
+        </Switch>
+      </Router>
+    );
+  }
+}
+
+export default Routes;
